fae7f61fa2bbafa4cb5bc392bb2cd89e8f2711d0
unknown <mark22312@gmail.com>
2018-02-11 22:32:13 +0200

    Removed Headers Log

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ var url = 'http://cp12.cdnwz.net/savta';
 
 icy.get(url, function (res) {
 
-  // log the HTTP response headers
-  console.error(res.headers);
-
   // log any "metadata" events that happen
   res.on('metadata', function (metadata) {
     var parsed = icy.parse(metadata);
