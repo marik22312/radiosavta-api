edbc8b69dadea3fd76085982a62a545cb97a00a4
unknown <mark22312@gmail.com>
2018-02-17 16:46:13 +0200

    Bugfixed - Removed irrelevant code - Syntax fixes - Offline cast handling

--- server.js
+++ server.js
@@ -2,33 +2,29 @@ const express = require('express');
 const icy = require('icy');
 
 const app = express();
-const port = process.env.PORT || 4800
+const port = process.env.PORT || 4800;
+const url = 'http://cp12.cdnwz.net/savta';
 
 app.listen(port, () => {
   console.log('Server on port', port);
 });
 // URL to a known Icecast stream
-var songName;
-var url = 'http://cp12.cdnwz.net/savta';
+var songName = {
+  StreamTitle: 'Sorry There\'s Something Wrong...'
+};
 
-
-icy.get(url, function (res) {
+icy.get(url, function(res) {
 
   // log any "metadata" events that happen
-  res.on('metadata', function (metadata) {
+  res.on('metadata', (metadata) => {
     var parsed = icy.parse(metadata);
-    console.error(parsed);
-    parsed ? songName = parsed : songName = 'Sorry There\'s Something Wrong...';
+    songName = parsed;
   });
   res.resume();
 });
 
 app.get('/', (req, res) => {
-  console.log('get /')
-
+  console.log('get /');
   res.set('Access-Control-Allow-Origin', '*');
-  res.send(songName.StreamTitle);
-
+  res.send(songName);
 });
-
-// http://cp12.cdnwz.net/savta
